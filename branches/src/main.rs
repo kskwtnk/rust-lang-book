e11dc5742f7bffa4091c9054c8a8d3a09da3012f
Keisuke Watanuki <watanuki@ju-zen.com>
2023-01-13 22:43:45 +0900

    Repeat code with "loop"

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,13 +1,21 @@
 fn main() {
-    let number = 6;
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
 
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 4, 3, or 2");
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
     }
+    println!("End count = {}", count);
 }
