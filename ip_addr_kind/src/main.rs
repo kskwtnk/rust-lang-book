6294dc3baff7f5903d56be6fda7a47b186fcecbe
Keisuke Watanuki <watanuki@ju-zen.com>
2023-01-27 23:30:14 +0900

    Define enum

--- ip_addr_kind/src/main.rs
+++ ip_addr_kind/src/main.rs
@@ -1,3 +1,11 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    println!("{:?}, {:?}", four, six);
 }
