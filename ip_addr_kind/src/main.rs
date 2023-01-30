4bb2301a32ff1b3599f98c84f7ef0af44932cf14
Keisuke Watanuki <watanuki@ju-zen.com>
2023-01-30 21:34:40 +0900

    Define IpAddr using only enum

--- ip_addr_kind/src/main.rs
+++ ip_addr_kind/src/main.rs
@@ -1,11 +1,12 @@
-#[derive(Debug)]
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-    println!("{:?}, {:?}", four, six);
+    #[derive(Debug)]
+    enum IpAddr {
+        V4(String),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    println!("{:?}, {:?}", home, loopback);
 }
