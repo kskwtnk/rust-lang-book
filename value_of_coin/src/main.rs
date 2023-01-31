d178f7eac5dbc594106f551af546a46321c540a7
Keisuke Watanuki <watanuki@ju-zen.com>
2023-01-31 22:57:23 +0900

    Determine the type of coin by match

--- value_of_coin/src/main.rs
+++ value_of_coin/src/main.rs
@@ -1,3 +1,23 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = Coin::Penny;
+    value_in_cents(x);
 }
