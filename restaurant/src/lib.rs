2a2449bd6b311253702285fd4f445ed2f20b7b10
Keisuke Watanuki <watanuki@ju-zen.com>
2023-02-04 20:17:19 +0900

    Expose paths with the pub keyword

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
