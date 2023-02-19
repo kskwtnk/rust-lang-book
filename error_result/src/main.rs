40a494d4e545ea53e5397d766687a2e99abe5e6f
Keisuke Watanuki <watanuki.keisuke@a-tm.co.jp>
2023-02-20 00:24:04 +0900

    Handle errors with `match`

--- error_result/src/main.rs
+++ error_result/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("hello.txt");
+
+    let _f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("There was a problem opening the file: {:?}", error)
+        },
+    };
 }
