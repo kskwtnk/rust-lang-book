be2089ac2f8f4661ed5a3b61f831ca1e35b58fc1
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2026-01-21 22:42:06 +0900

    4.3. The Slice Type

--- ownership_rules/src/main.rs
+++ ownership_rules/src/main.rs
@@ -1,8 +1,23 @@
 fn main() {
-    let reference_to_nothing = dangle();
+    let my_string = String::from("hello world");
+
+    let word = first_word(&my_string[..]);
+
+    let my_string_literal = "hello world";
+
+    let word = first_word(&my_string_literal[..]);
+
+    let word = first_word(my_string_literal);
 }
 
-fn dangle() -> String {
-    let s = String::from("hello");
-    s
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
